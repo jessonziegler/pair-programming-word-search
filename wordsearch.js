db5c6db7120a2d654955b6c1b37aa275f3160662
jessonziegler <jziegleraudio@gmail.com>
2021-09-22 21:52:12 +0000

    pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
+
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+          return true;
+        }
     }
-}
 
-module.exports = wordSearch
+    let nestedArray = [];
+    for (let i = 0; i <letters[0].length; i++) {
+      let tempArray = [];
+      for (let j = 0; j < letters.length; j++) {
+        tempArray.push(letters[j][i]);
+      }
+      nestedArray.push(tempArray);
+    }
+
+    const verticalJoin = nestedArray.map(ls => ls.join(''));
+    for(let l of verticalJoin) {
+      if (l.includes(word)) return true;
+    }
+    return false;
+  }
+
+  module.exports = wordSearch;
